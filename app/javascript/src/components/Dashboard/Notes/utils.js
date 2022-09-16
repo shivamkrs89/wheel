513ece5be0a133a5f8ccc6a04a0dd6c98bc57216
shivamkrs89 <bigbinary@BigBinarys-MacBook-Pro-2.local>
2022-09-16 19:50:23 +0530

    PR fixes based on lib/dayjs imports

--- app/javascript/src/components/Dashboard/Notes/utils.js
+++ app/javascript/src/components/Dashboard/Notes/utils.js
@@ -1,5 +1,5 @@
-import dayjs from "lib/dayjs";
+import dayjs from "dayjs";
 
-export const weekDaydateFormat = date => dayjs(date).format("dddd, hh:mmA");
+export const formatDatetoweekDay = date => dayjs(date).format("dddd, hh:mmA");
 
 export const calculateCreatedAgo = date => dayjs(date).fromNow();
